3d28f4b20c3581e4bca08d8f502e24d64da9bfd4
reivaxFast <xjehoward@gmail.com>
2025-04-30 10:47:49 +0100

    added database with proper tables

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request, session, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -5,26 +6,26 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+class user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     username = db.Column(db.String(150), nullable=False)
     DOB = db.Column(db.Date, nullable=True)
     password_hash = db.Column(db.String(150), nullable=False)
     
-    tasks = db.relationship('SetTasks', backref='user', lazy=True)
+    tasks = db.relationship('set_tasks', backref='user', lazy=True)
     home_owner = db.relationship('Homes', backref='owner', lazy=True)
-    homes = db.relationship('UserInHome', backref='user', lazy=True)
+    homes = db.relationship('user_in_home', backref='user', lazy=True)
     rota_set = db.relationship('Rotas', backref='set', lazy=True)
-    rota = db.relationship('RotaUsers', backref='user', lazy=True)
+    rota = db.relationship('rota_users', backref='user', lazy=True)
     photo_uploader = db.relationship('Photos', backref='uploader', lazy=True)
     file_uploader = db.relationship('Files', backref='uploader', lazy=True)
-    pinboard_creator = db.relationship('PinboardItems', backref='creator', lazy=True)
-    tasks_creator = db.relationship('SetTasks', backref='creator', lazy=True)
+    pinboard_creator = db.relationship('pinboard_items', backref='creator', lazy=True)
+    tasks_creator = db.relationship('set_tasks', backref='creator', lazy=True)
     
     def update_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -32,22 +33,22 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-class SetTasks(db.Model):
+class set_tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
+    home_id = db.Column(db.Integer, db.ForeignKey('homes.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     task_name = db.Column(db.String(200), nullable=False)
     date_set = db.Column(db.DateTime, default=datetime.utcnow)
     date_due = db.Column(db.DateTime, nullable=True)
     description = db.Column(db.String(1000), nullable=True)
     set_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
-    attachment = db.relationship('TaskAttachments', backref='task', lazy=True)
+    attachment = db.relationship('task_attachments', backref='task', lazy=True)
 
-class TaskAttachments(db.Model):
-    task_id = db.Column(db.Integer, db.ForeignKey('SetTasks.id'), primary_key=True)
-    attachment_id = db.Column(db.Integer, db.ForeignKey('Files.id'), primary_key=True)
+class task_attachments(db.Model):
+    task_id = db.Column(db.Integer, db.ForeignKey('set_tasks.id'), primary_key=True)
+    attachment_id = db.Column(db.Integer, db.ForeignKey('files.id'), primary_key=True)
 
-class Homes(db.Model):
+class homes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     home_name = db.Column(db.String(200), nullable=False)
@@ -55,8 +56,8 @@ class Homes(db.Model):
     join_by_link = db.Column(db.Boolean, default=False)
     home_password = db.Column(db.String(200), nullable=True)
     background_photo = db.Column(db.LargeBinary, nullable=True)
-    tasks = db.relationship('SetTasks', backref='home', lazy=True)
-    users = db.relationship('UserInHome', backref='home', lazy=True)
+    tasks = db.relationship('set_tasks', backref='home', lazy=True)
+    users = db.relationship('user_in_home', backref='home', lazy=True)
     photos = db.relationship('Photos', backref='home', lazy=True)
     pinboard = db.relationship('Pinboard', backref='home', lazy=True)
     lists = db.relationship('Lists', backref='home', lazy=True) 
@@ -64,58 +65,58 @@ class Homes(db.Model):
     files = db.relationship('Files', backref='home', lazy=True) 
     rotas = db.relationship('Rotas', backref='home', lazy=True) 
 
-class UserInHome(db.Model):
+class user_in_home(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), primary_key=True)
+    home_id = db.Column(db.Integer, db.ForeignKey('homes.id'), primary_key=True)
     is_admin = db.Column(db.Boolean, default=False)
 
-class Rotas(db.Model):
+class rotas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
+    home_id = db.Column(db.Integer, db.ForeignKey('homes.id'), nullable=False)
     rota_name = db.Column(db.String(200), nullable=False)
     description = db.Column(db.String(1000), nullable=True)
     last_changed = db.Column(db.DateTime, default=datetime.utcnow)
     set_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
     pointer = db.Column(db.Integer, nullable=True)
     size = db.Column(db.Integer, nullable=True)
-    users = db.relationship('RotaUsers', backref='rota', lazy=True)
+    users = db.relationship('rota_users', backref='rota', lazy=True)
 
-class RotaUsers(db.Model):
+class rota_users(db.Model):
     rota_id = db.Column(db.Integer, db.ForeignKey('rotas.id'), primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     position = db.Column(db.Integer, nullable=False)
     catchup = db.Column(db.Integer, nullable=False)
 
-class Lists(db.Model):
+class lists(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
+    home_id = db.Column(db.Integer, db.ForeignKey('homes.id'), nullable=False)
     name = db.Column(db.String(200), nullable=False)
     admin_only = db.Column(db.Boolean, default=False)
-    item = db.relationship('ListItems', backref='list', lazy=True)
+    item = db.relationship('list_items', backref='list', lazy=True)
 
-class ListItems(db.Model):
+class list_items(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    list_id = db.Column(db.Integer, db.ForeignKey('Lists.id'), nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('lists.id'), nullable=False)
     name = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
 
-class Photos(db.Model):
-    photo_id = db.Column(db.Integer, primary_key=True)
-    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
+class photos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    home_id = db.Column(db.Integer, db.ForeignKey('homes.id'), nullable=False)
     uploader_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     photo = db.Column(db.LargeBinary, nullable=False)
     date_uploaded = db.Column(db.DateTime, default=datetime.utcnow)
-    event = db.relationship('EventPhotos', backref='photo', lazy=True)
+    event = db.relationship('event_photos', backref='photo', lazy=True)
 
-class Pinboard(db.Model):
-    pinboard_id = db.Column(db.Integer, primary_key=True)
-    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
-    admin_only = db.Column(db.Boolean, default=False)
-    item = db.relationship('PinboardItems', backref='pinboard', lazy=True)
-
-class PinboardItems(db.Model):
+class pinboard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    pinboard_id = db.Column(db.Integer, db.ForeignKey('Pinboard.id'), nullable=False)
+    home_id = db.Column(db.Integer, db.ForeignKey('homes.id'), nullable=False)
+    admin_only = db.Column(db.Boolean, default=False)
+    item = db.relationship('pinboard_items', backref='pinboard', lazy=True)
+
+class pinboard_items(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pinboard_id = db.Column(db.Integer, db.ForeignKey('pinboard.id'), nullable=False)
     creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(500), nullable=True)
@@ -124,30 +125,30 @@ class PinboardItems(db.Model):
     size_x = db.Column(db.Integer, nullable=False)
     size_y = db.Column(db.Integer, nullable=False)
 
-class PinboardAttachments(db.Model):
+class pinboard_attachments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    item_id = db.Column(db.Integer, db.ForeignKey('PinboardItems.id'), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('pinboard_items.id'), nullable=False)
     file_id = db.Column(db.Integer, db.ForeignKey('files.id'), nullable=False)
 
-class Files(db.Model):
+class files(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
+    home_id = db.Column(db.Integer, db.ForeignKey('homes.id'), nullable=False)
     uploader_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     file = db.Column(db.LargeBinary, nullable=False)
     date_uploaded = db.Column(db.DateTime, default=datetime.utcnow)
 
-class Event(db.Model):
-    event_id = db.Column(db.Integer, primary_key=True)
-    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
+class event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    home_id = db.Column(db.Integer, db.ForeignKey('homes.id'), nullable=False)
     name = db.Column(db.String(200), nullable=False)
     description = db.Column(db.String(400), nullable=True)
     start = db.Column(db.DateTime, nullable=False)
     end = db.Column(db.DateTime, nullable=False)
-    photo = db.relationship('EventPhotos', backref='event', lazy=True)
+    photo = db.relationship('event_photos', backref='event', lazy=True)
 
-class EventPhotos(db.Model):
-    event_id = db.Column(db.Integer, db.ForeignKey('Event.id'), primary_key=True)
-    photo_id = db.Column(db.Integer, db.ForeignKey('Photos.id'), primary_key=True)
+class event_photos(db.Model):
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), primary_key=True)
+    photo_id = db.Column(db.Integer, db.ForeignKey('photos.id'), primary_key=True)
 
 
 @app.route('/')
@@ -161,14 +162,31 @@ def register():
     if request.method == 'POST':
         email = request.form['email']
         username = request.form['username']
-        password = request.form['password']
+        password = request.form['password1']
+        password2 = request.form['password2']
+        if password != password2:
+            return render_template('register.html', error="Passwords do not match", disp='block')
+        if User.query.filter_by(email=email).first():
+            return render_template('register.html', error="Email already exists", disp='block')
+        if not email or not username or not password:
+            return render_template('register.html', error="All fields are required", disp='block')
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            return render_template('register.html', error="Invalid email format", disp='block')
+        if len(password) < 8:
+            return render_template('register.html', error="Password must be at least 8 characters long", disp='block')
+        if not any(char.isdigit() for char in password):
+            return render_template('register.html', error="Password must contain at least one digit", disp='block')
+        if not any(char.isalpha() for char in password):
+            return render_template('register.html', error="Password must contain at least one letter", disp='block')
+        if not any(char in "!@#$%^&*()-_+=<>?/|{}[]:;'" for char in password):
+            return render_template('register.html', error="Password must contain at least one special character", disp='block')
         dob = request.form['DOB']
         hashed_password = generate_password_hash(password, method='sha256')
         new_user = User(email=email, DOB = dob, username=username, password_hash=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
-    return render_template('register.html')
+    return render_template('register.html', error=None, disp='none')
 
 @app.route('/logout')
 def logout():
